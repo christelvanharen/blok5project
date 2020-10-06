32d7ae7e4df6e199b4d8e346099367ae5d3e0386
christelvanharen <cvanharen@outlook.com>
2020-10-06 14:48:37 +0200

    bash

--- iteraties.sh
+++ iteraties.sh
@@ -1,7 +1,12 @@
-for number in 1 2 3
-do python3 pipeline.py thioredoxine_swissprot.fasta muscle_swissprot.fasta hmmbuild_swissprot_muscle hmmsearch_swissprot uniprot_sprot.fasta
-do python3 pipeline.py thioredoxine_pdb.fasta muscle_pdb.fasta hmmbuild_pdb_muscle hmmsearch_pdb pdb_seqres.fasta
-
-
-
-
+for number in 1 2 3 4
+do
+python3 pipeline.py thioredoxine_swissprot.fasta muscle_swissprot.fasta hmmbuil>
+echo "Number: $number"
+if [number -eq 4] ; then
+break
+done
+do
+muscle -in thioredoxine_swissprot.fasta -out muscle_swissprot.fasta
+hmmbuild --amino muscle_swissprot.fasta final_product
+echo "idioot"
+done
