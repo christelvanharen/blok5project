94d54a4008240b3aa93f41713db879e5d6c5335e
D-Hildebrand <55357685+D-Hildebrand@users.noreply.github.com>
2020-10-07 15:51:03 +0200

    Add files via upload
    werkende bash

--- iteraties.sh
+++ iteraties.sh
@@ -1,13 +1,24 @@
-for number in 1 2 3 4
-do
-python3 pipeline.py thioredoxine_swissprot.fasta muscle_swissprot.fasta hmmbuild_swissprot_muscle hmmsearch_swissprot uniprot_sprot.fasta
+#!/bin/bash
+for number in {1..3}
+do 
+
+if [ "$number" -eq "1" ] ; then   #Number wordt hier niet herkend als een variabele
+    mv thioredoxine_swissprot.fasta thioredoxine_swissprot$number.fasta
+    
+    else
+    previousnum = $number-1
+    mv hmmsearch_swissprot$previousnum thioredoxine_swissprot$number.fasta   #hmmsearch_swissprot$number - $number moet -1 zijn om de vorige iter te pakken
+    fi
+
+python3 pipeline.py thioredoxine_swissprot$number.fasta muscle_swissprot$number.fasta hmmbuild_swissprot_muscle$number hmmsearch_swissprot$number uniprot_sprot.fasta $number
+
+
 echo "Number: $number"
-if [number -eq 4] ; then
-break
-done
-do
-muscle -in thioredoxine_swissprot.fasta -out muscle_swissprot.fasta
-hmmbuild --amino muscle_swissprot.fasta final_product
-echo "idioot"
+
 done
 
+# do 
+# muscle -in thioredoxine_swissprot.fasta -out muscle_swissprot.fasta
+# hmmbuild --amino muscle_swissprot.fasta final_product
+
+# done
