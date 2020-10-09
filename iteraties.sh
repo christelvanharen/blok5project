3df3fe3311078ff3d053ff1f32b1bf44a3a6fdfa
D-Hildebrand <55357685+D-Hildebrand@users.noreply.github.com>
2020-10-09 14:33:09 +0200

    Add files via upload

--- iteraties.sh
+++ iteraties.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
+
+# @brief 
+# Loops together with pipeline.py script iterates three times to find orthologues using locally downloaded uniprot db, aligns these and creates HMM's
+# Requires several files:
+# pipeline.py
+# uniprot_sprot.fasta
+
+
+
+echo "Type the name of the file you want to iterate. Input is BLAST fasta output."
+inputfile="USER INPUT"
+read -p "Enter input file: " inputfile
+
+
+# For three loops, number is a variable
 for number in {1..3}
 do 
 
+# For first iteration, rename first file to have a number
 if [ "$number" -eq "1" ] ; then
-    mv thioredoxine_swissprot.fasta thioredoxine_swissprot$number.fasta
+    mv $inputfile researchfileloop$number.fasta
     
+# For other loops, take hmmsearch file of previous loop, change the name for next loop
     else
     previousnum=$((number-1))
-    mv hmmsearch_swissprot$previousnum.fasta thioredoxine_swissprot$number.fasta
+    mv hmmsearchloop$previousnum.fasta researchfileloop$number.fasta
     fi
 
-python3 pipeline.py thioredoxine_swissprot$number.fasta muscle_swissprot$number.fasta hmmbuild_swissprot_muscle$number hmmsearch_swissprot$number uniprot_sprot.fasta $number
-esl-reformat -o hmmsearch_swissprot$number.fasta fasta hmmsearch_swissprot$number
+# Calling python program for file processing
+python3 pipeline.py researchfileloop$number.fasta muscleloop$number.fasta hmmbuildloop$number hmmsearchloop$number uniprot_sprot.fasta $number
 
-echo "Number: $number"
+# Change format from .sto to .fasta
+esl-reformat -o hmmsearchloop$number.fasta fasta hmmsearchloop$number
+
+# Print in terminal current running iteration
+echo "Iteration no: $number"
 
 done
 
-# do 
-# muscle -in thioredoxine_swissprot.fasta -out muscle_swissprot.fasta
-# hmmbuild --amino muscle_swissprot.fasta final_product
+# Align final iteration and produce hmm profile
 
-# done
+muscle -in researchfileloop3.fasta -out muscle4.fasta
+hmmbuild --amino hmmbuild_final_product muscle4.fasta
+
+
